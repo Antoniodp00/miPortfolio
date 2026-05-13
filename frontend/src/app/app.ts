133637fa1ca513393a1d12732dbb3c99aaad7efd
Antoniodp00 <antoniodelgadoportero070100@gmail.com>
2026-05-13 07:41:35 +0200

    P1: lazy-load sections (@defer), remove admin dead code, a11y improvements, minimalist section labels

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { TranslatePipe } from './pipes/translate.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, TranslatePipe],
   template: `
+    <a class="skip-link" href="#main">{{ 'a11y.skipToContent' | translate }}</a>
     <app-navbar />
-    <main>
+    <main id="main" tabindex="-1">
       <router-outlet />
     </main>
     <app-footer />
